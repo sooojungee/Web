a2062a497c741400d16144d0b951cfe7c7880d48
Soojung Lee <sooojungee@gmail.com>
2018-06-18 14:41:58 +0900

    first commit

---
+++ routes/users.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/test', function(req, res, next) {
+    res.render('test', {title: '아주대에 오신걸 환영합니다.'});
+});
+
+
+router.get('/modal', function(req, res, next) {
+    res.render('modal', {title: 'HELLO!'});
+});
+
+router.get('/instagram', function(req, res, next) {
+    res.render('instagram', {title: 'HELLO!'});
+});
+
+router.get('/insta', function(req, res, next) {
+    res.render('example', {title: 'SHINEE KEY(@bumkeyk)'});
+});
+
+
+router.get('/test0608', function(req, res, next) {
+    res.render('test0608', {title: 'asdf'});
+});
+
+router.get('/kakaotalk', function(req, res, next) {
+    res.render('kakaomain', {title: '카카오톡'});
+});
+
+router.get('/kakao', function(req, res, next) {
+    res.render('kakaomain', {title: '로그인'});
+});
+
+router.get('/kakaofriend', function(req, res, next) {
+    res.render('kakaofriend', {title: '카카오톡2'});
+});
+
+router.get('/kakaochat', function(req, res, next) {
+    res.render('kakaochat', {title: '카카오톡2'});
+});
+
+router.get('/kakaosetting', function(req, res, next) {
+    res.render('kakaosetting', {title: '카카오톡2'});
+});
+
+router.get('/kakaouserchat', function(req, res, next) {
+    res.render('kakaouserchat', {title: '카카오톡2'});
+});
+
+router.get('/kakaoprofile', function(req, res, next) {
+    res.render('kakaoprofile', {title: '카카오톡2'});
+});
+
+module.exports = router;
